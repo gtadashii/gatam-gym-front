bd3d822e21ab694812dc0a21bedaf1fe130449db
matheusmks <matheusstreppel99@gmail.com>
2022-08-28 16:14:08 -0300

    feat: adds Alisson Alert component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Container, Navbar } from "react-bootstrap";
 import { WorkoutModal } from "./components/WorkoutModal"
-import "./App.css";
+import { Alert } from "../src/components/Alert";
 
 function App() {
 
   return (
     <>
-        <Navbar bg="dark" expand="lg">
-          <Container fluid>
-            <Navbar.Brand href="#">GATAM GYM</Navbar.Brand>
-            <WorkoutModal />
-          </Container>
-        </Navbar>
-        <h1>Hello, world!</h1>
+      <Navbar bg="dark" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Sistema GATAM</Navbar.Brand>
+          <WorkoutModal />
+        </Container>
+      </Navbar>
+      <h1>Hello, world!</h1>
+      <Alert />
     </>
   );
 }
