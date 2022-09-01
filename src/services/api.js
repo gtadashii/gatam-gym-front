81f384e44bad44f7d12ee626687bdfa5502e76e5
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2022-09-01 20:34:23 -0300

    fix: api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ajlgff9h51.execute-api.us-east-1.amazonaws.com/dev",
+  baseURL: "https://7g5fxxelh9.execute-api.us-east-1.amazonaws.com",
 });
 
 export default api;
