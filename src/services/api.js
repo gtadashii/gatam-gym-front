3a2a82aeb17cf307c9267e4221c79087fad2d0a6
Guilherme Tadashii <57333585+gtadashii@users.noreply.github.com>
2022-10-22 16:51:54 -0300

    chore: updates api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://3g7bnwacle.execute-api.us-east-1.amazonaws.com/dev",
+  baseURL: "https://sjsvo656s5.execute-api.us-east-1.amazonaws.com/",
 });
 
 export default api;
